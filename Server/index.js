9fbc90f3c38e02dbe14404d29a10f0e532017854
londtreeforce <kossfiedberg@gmail.com>
2023-04-13 14:49:58 +0300

    Express Server

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+
+const route = require('./route');
+
+app.use(cors({ origin: "*" }));
+app.use(route);
+
+
+
+const io = new Server(server , {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+
+
+
+
+server.listen(5000, () => {
+    console.log("Server started");
+});
+
+
