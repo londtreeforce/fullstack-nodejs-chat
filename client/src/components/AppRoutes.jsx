995af6b9609efcb7c6a5be100f70645c99c6e6a9
londtreeforce <kossfiedberg@gmail.com>
2023-04-13 15:52:33 +0300

    Client stability version

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Body from './Body';
+import Chat from './Chat';
 
 const AppRoutes = () => {
   return(
     <Routes>
       <Route path="/" element={<Body/>}/>
+      <Route path="/chat" element={<Chat/>}/>
     </Routes>
   )
 };
--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    
+`
+
+const Heading = styled.h1`
+    
+`
+
+const Form = styled.form`
+    
+`
+const Group = styled.div`
+    
+`
+
+const Input = styled.input`
+    
+`
+
+const Button = styled.button`
+    
+`
+
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <Container>
+        <Heading>Entry</Heading>
+        <Form>
+            <Group>
+                <Input 
+                type="text" 
+                name="username" 
+                value=""
+                onChange={() => {}} 
+                autoComplete="off"
+                placeholder="Username"/>
+            </Group>
+            <Group>
+                <Input 
+                type="text" 
+                name="room" 
+                value=""
+                onChange={() => {}} 
+                autoComplete="off"
+                placeholder="Room"/>
+            </Group>
+
+
+            <Link to={'/chat?'}>
+                <Button type="submit">Sign</Button>    
+            </Link> 
+
+
+        </Form>
+    </Container>
   )
 }
 
